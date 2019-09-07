e00311dcbcd9bcd7e0df29bd130110efe0649e54
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-07 20:12:18 -0300

    eventos com lógica + tratamento de erros

---
+++ javaFX4/src/gui/ViewController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.util.Locale;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+
+	@FXML
+	private TextField txtNum1;
+
+	@FXML
+	private TextField txtNum2;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btSum;	
+
+	@FXML
+	public void onBtSumAction() {
+		Locale.setDefault(Locale.US);
+
+		if(txtNum1.getText().isEmpty() || txtNum2.getText().isEmpty()) {
+			Alerts.showAlert("Aviso!", "Dados não preenchidos", "Favor preencher os campos", AlertType.INFORMATION);
+		}else {
+			try {
+				double number1 = Double.parseDouble(txtNum1.getText());
+				double number2 = Double.parseDouble(txtNum2.getText());
+				double result  = number1 + number2;
+
+				labelResult.setText(String.format("%.2f", result));
+			}catch (NumberFormatException e) {
+				Alerts.showAlert("Erro", null, "Você digitou um carcater que não é um número", AlertType.ERROR);
+				txtNum1.setText(null);
+				txtNum2.setText(null);
+				labelResult.setText(null);
+			}
+		}
+
+	}
+
+}
