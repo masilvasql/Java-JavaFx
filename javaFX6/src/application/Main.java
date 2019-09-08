4299c57f44201784158bb209c45b83d1ff303439
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-07 23:50:29 -0300

    comboBox

--- javaFX6/src/application/Main.java
+++ javaFX6/src/application/Main.java
@@ -23,18 +23,7 @@ public class Main extends Application {
 			e.printStackTrace();  
 		} 
 	}
-//	@Override
-//	public void start(Stage primaryStage) {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+
 	
 	public static void main(String[] args) {
 		launch(args);
