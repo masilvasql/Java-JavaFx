fe1e19fe8ae8f9eefe6a0d5631a2f9af0bdd6cbb
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-08 15:26:20 -0300

    ajustando tela inicial com a menuBar

--- Workshop-jfx-jdbc/src/application/Main.java
+++ Workshop-jfx-jdbc/src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,14 +14,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollpane = loader.load();
+			
+			scrollpane.setFitToHeight(true); // ajusta o scrollpane na altura da tela
+			scrollpane.setFitToWidth(true);  //ajusta o scrollpane na largura da tela
+			
+			Scene mainScene = new Scene(scrollpane);
 			
 			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.setTitle("Sample JavaFX application");	
 			primaryStage.show();
 			
 		} catch (IOException e) {
