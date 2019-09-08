619f9a22795bdf6bfcbf9276c2fcdcc56b9eadd4
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-08 16:13:57 -0300

    adicionado department list

--- Workshop-jfx-jdbc/src/application/Main.java
+++ Workshop-jfx-jdbc/src/application/Main.java
@@ -11,35 +11,30 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	
 	private static Scene mainScene;
-
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollpane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			scrollpane.setFitToHeight(true); // ajusta o scrollpane na altura da tela
-			scrollpane.setFitToWidth(true);  //ajusta o scrollpane na largura da tela
-		
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
-			mainScene = new Scene(scrollpane);
-			
-			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.setMaximized(true);
 			primaryStage.show();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Scene getMainScene() {
 		return mainScene;
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
