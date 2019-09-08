621ee14ab58ebf0c7f34db52227914bdb4cbb595
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-08 15:16:57 -0300

    criando mainView

--- javaFX6/src/gui/ViewController.java
+++ javaFX6/src/gui/ViewController.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.util.Callback;
 import model.entities.Person;
 
@@ -62,6 +65,8 @@ public class ViewController implements Initializable {
 
 		comboBoxPerson.setCellFactory(factory);
 		comboBoxPerson.setButtonCell(factory.call(null));
+		
+
 
 	}
 
