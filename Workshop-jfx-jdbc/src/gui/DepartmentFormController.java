3f16a3cd6bcc4d04216049fecca75a83ffe62e3f
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-12 21:52:02 -0300

    passando objeto departamento para departmentform

--- Workshop-jfx-jdbc/src/gui/DepartmentFormController.java
+++ Workshop-jfx-jdbc/src/gui/DepartmentFormController.java
@@ -9,45 +9,60 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
-public class DepartmentFormController implements Initializable{
-	
+public class DepartmentFormController implements Initializable {
+
+	private Department entity;
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btnSave;
-	
+
 	@FXML
 	private Button btnCancel;
-	
+
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
 	@FXML
 	public void onBtnSaveAction() {
 		System.out.println("SAVE");
 	}
-	
+
 	@FXML
 	public void onBtnCancelAction() {
 		System.out.println("CANCEL");
 	}
-	
-	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		intializeNodes();
-		
+
 	}
-	
+
 	private void intializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("entity was null");
+		}else {
+			txtId.setText(String.valueOf(entity.getId()));
+			txtName.setText(entity.getName());
+		}
+
 	}
 
 }
